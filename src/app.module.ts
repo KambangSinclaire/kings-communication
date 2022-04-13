ae483c46142028c7a693f2d6e82f9862b2672a6c
Kambang Sinclaire <kambangsinclaire@gmail.com>
2022-04-13 12:52:19 +0100

    modules setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageModule } from './modules/message/message.module';
+import { CallsModule } from './modules/calls/calls.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [MessageModule, CallsModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
